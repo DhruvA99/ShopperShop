79ed0c51dbf914f52d8fbb58da0f22b0c43ff3e3
Dhruv <dhruvarora9931@gmail.com>
2021-01-26 21:27:51 +0530

    feat(payment.js):Added card payment option and handled validation

--- src/container/Payment/Payment.js
+++ src/container/Payment/Payment.js
@@ -4,6 +4,8 @@ import { CountryDropdown, RegionDropdown } from "react-country-region-selector";
 import Navbar from "../../components/Navigation/Navbar";
 import { connect } from "react-redux";
 
+var reg = /^\d+$/;
+
 class Payment extends React.Component {
   state = {
     addLine1: "",
@@ -13,11 +15,21 @@ class Payment extends React.Component {
     country: "",
     region: "",
     mobileNo: null,
+    cardName: "",
+    cardNo: "",
+    expMonth: null,
+    expYear: null,
+    cvv: "",
     isValid: false,
     error: {
       addLine1: "",
       zipcode: "",
       mobileNo: "",
+      cardName: "",
+      cardNo: "",
+      expMonth: "",
+      expYear: "",
+      cvv: "",
     },
   };
 
@@ -41,6 +53,50 @@ class Payment extends React.Component {
             ? ""
             : "zipcode must not be empty or incorrect!";
         break;
+      case "cardName":
+        errors.cardName = value.length < 1 ? "Name cannot be empty! " : "";
+        break;
+      case "cardNo":
+        errors.cardNo =
+          value.toString().length < 1
+            ? "cardNo must not be empty!"
+            : reg.test(value.toString())
+            ? value.toString().length === 12
+              ? ""
+              : "cardNo not valid"
+            : "CardNo can only be numeric";
+        break;
+      case "expMonth":
+        errors.expMonth =
+          value.toString().length < 1
+            ? "expMonth must not be empty!"
+            : reg.test(value.toString())
+            ? parseInt(value.toString()) <= 12 && value.toString().length === 2
+              ? ""
+              : "expMonth not valid"
+            : "expMonth can only be numeric";
+        break;
+      case "expYear":
+        errors.expYear =
+          value.toString().length < 1
+            ? "expYear must not be empty!"
+            : reg.test(value.toString())
+            ? parseInt(value.toString()) <= 2050 &&
+              value.toString().length === 4
+              ? ""
+              : "expYear not valid"
+            : "expYear can only be numeric";
+        break;
+      case "cvv":
+        errors.cvv =
+          value.toString().length < 1
+            ? "CVV must not be empty"
+            : reg.test(value.toString())
+            ? value.toString().length === 3
+              ? ""
+              : "CVV not valid"
+            : "CVV can only be numeric";
+        break;
       default:
         break;
     }
@@ -48,11 +104,21 @@ class Payment extends React.Component {
       errors.zipcode === "" &&
       errors.addLine1 === "" &&
       errors.mobileNo === "" &&
+      errors.cardNo === "" &&
+      errors.cardName === "" &&
+      errors.expMonth === "" &&
+      errors.expYear === "" &&
+      errors.cvv === "" &&
       this.state.addLine1 !== null &&
       this.state.zipcode !== null &&
       this.state.country !== null &&
       this.state.region !== null &&
-      this.state.mobileNo !== null
+      this.state.mobileNo !== null &&
+      this.state.cardNo !== null &&
+      this.state.cardName !== null &&
+      this.state.expMonth !== null &&
+      this.state.expYear !== null &&
+      this.state.cvv !== null
     ) {
       Valid = true;
     }
@@ -84,90 +150,225 @@ class Payment extends React.Component {
       <>
         <Navbar />
         <div className={classes.main}>
-          <h1>YOUR DETAILS</h1>
-          <div className={classes.addressMain}>
-            <div className={classes.inputDiv}>
-              <span className={classes.sideText}>Address Line 1 *</span>
-              <input
-                className={classes.input}
-                type="text"
-                name="addLine1"
-                value={this.state.addLine1}
-                onChange={(e) => this.onChangeHandler(e)}
-              />
-            </div>
-            <span style={{ marginBottom: "5px", color: "red" }}>
-              {this.state.error.addLine1}
-            </span>
-            <div className={classes.inputDiv}>
-              <span className={classes.sideText}>Address Line 2(opt.)</span>
-              <input
-                className={classes.input}
-                type="text"
-                name="addLine2"
-                value={this.state.addLine2}
-                onChange={(e) => this.onChangeHandler(e)}
-              />
-            </div>
-            <div className={classes.inputDiv}>
-              <span className={classes.sideText}>Address Line 3(opt.)</span>
-              <input
-                className={classes.input}
-                type="text"
-                name="addLine3"
-                value={this.state.addLine3}
-                onChange={(e) => this.onChangeHandler(e)}
-              />
-            </div>
+          <div className={classes.mainFlexContainer}>
+            <div className={classes.addressMain}>
+              <span className={classes.heading}>BILLING ADDRESS</span>
+              <div className={classes.inputDiv}>
+                <span className={classes.sideText}>Address Line 1 *</span>
+                <input
+                  className={classes.input}
+                  type="text"
+                  name="addLine1"
+                  value={this.state.addLine1}
+                  onChange={(e) => this.onChangeHandler(e)}
+                />
+              </div>
+              <span style={{ marginBottom: "5px", color: "red" }}>
+                {this.state.error.addLine1}
+              </span>
+              <div className={classes.inputDiv}>
+                <span className={classes.sideText}>Address Line 2(opt.)</span>
+                <input
+                  className={classes.input}
+                  type="text"
+                  name="addLine2"
+                  value={this.state.addLine2}
+                  onChange={(e) => this.onChangeHandler(e)}
+                />
+              </div>
+              <div className={classes.inputDiv}>
+                <span className={classes.sideText}>Address Line 3(opt.)</span>
+                <input
+                  className={classes.input}
+                  type="text"
+                  name="addLine3"
+                  value={this.state.addLine3}
+                  onChange={(e) => this.onChangeHandler(e)}
+                />
+              </div>
 
-            <div className={classes.inputDiv}>
-              <span className={classes.sideText}>Country *</span>
-              <CountryDropdown
-                className={classes.input}
-                value={this.state.country}
-                name="country"
-                onChange={(val) => this.selectCountry(val)}
-              />
+              <div className={classes.inputDiv}>
+                <span className={classes.sideText}>Country *</span>
+                <CountryDropdown
+                  className={classes.input}
+                  value={this.state.country}
+                  name="country"
+                  onChange={(val) => this.selectCountry(val)}
+                />
+              </div>
+              <div className={classes.inputDiv}>
+                <span className={classes.sideText}>Region *</span>
+                <RegionDropdown
+                  className={classes.input}
+                  value={this.state.region}
+                  country={this.state.country}
+                  name="region"
+                  onChange={(val) => this.selectRegion(val)}
+                />
+              </div>
+              <div className={classes.inputDiv}>
+                <span className={classes.sideText}>zip code *</span>
+                <input
+                  className={classes.input}
+                  type="number"
+                  name="zipcode"
+                  value={this.state.zipcode}
+                  onChange={(e) => this.onChangeHandler(e)}
+                />
+              </div>
+              <span style={{ marginBottom: "5px", color: "red" }}>
+                {this.state.error.zipcode}
+              </span>
+              <div className={classes.inputDiv}>
+                <span className={classes.sideText}>Mobile No(with Std) *</span>
+                <input
+                  className={classes.input}
+                  type="number"
+                  name="mobileNo"
+                  value={this.state.mobileNo}
+                  onChange={(e) => this.onChangeHandler(e)}
+                />
+              </div>
+              <span style={{ marginBottom: "5px", color: "red" }}>
+                {this.state.error.mobileNo}
+              </span>
             </div>
-            <div className={classes.inputDiv}>
-              <span className={classes.sideText}>Region *</span>
-              <RegionDropdown
-                className={classes.input}
-                value={this.state.region}
-                country={this.state.country}
-                name="region"
-                onChange={(val) => this.selectRegion(val)}
-              />
+            <div className={classes.paymentContainer}>
+              <span className={classes.heading}>PAYMENT</span>
+              <div className={classes.paymentContainer}>
+                <span className={classes.sideText}>Accepted Cards</span>
+                <div className={classes.cardsIconContainer}></div>
+                <div className={classes.inputDiv}>
+                  <span className={classes.sideText}>Name on Card</span>
+                  <div className={classes.insideInputDiv}>
+                    <input
+                      className={classes.input}
+                      name="cardName"
+                      value={this.state.cardName}
+                      type="text"
+                      placeholder="John Snow"
+                      onChange={(e) => this.onChangeHandler(e)}
+                    />
+                    <span
+                      style={{
+                        marginBottom: "5px",
+                        textAlign: "center",
+                        color: "red",
+                      }}
+                    >
+                      {this.state.error.cardName}
+                    </span>
+                  </div>
+                </div>
+
+                <div className={classes.inputDiv}>
+                  <span className={classes.sideText}>Credit Card number</span>
+                  <div className={classes.insideInputDiv}>
+                    {" "}
+                    <input
+                      className={classes.input}
+                      type="text"
+                      name="cardNo"
+                      value={this.state.cardNo}
+                      placeholder="1111-2222-3333-4444"
+                      maxLength="12"
+                      onChange={(e) => this.onChangeHandler(e)}
+                    />{" "}
+                    <span
+                      style={{
+                        marginBottom: "5px",
+                        color: "red",
+                        textAlign: "center",
+                      }}
+                    >
+                      {this.state.error.cardNo}
+                    </span>
+                  </div>
+                </div>
+
+                <div className={classes.inputDiv}>
+                  <span className={classes.sideText}>Expiry Month</span>
+                  <div className={classes.insideInputDiv}>
+                    <input
+                      className={classes.input}
+                      type="text"
+                      name="expMonth"
+                      value={this.state.expMonth}
+                      maxLength="2"
+                      placeholder="03"
+                      onChange={(e) => this.onChangeHandler(e)}
+                    />{" "}
+                    <span
+                      style={{
+                        marginBottom: "5px",
+                        color: "red",
+                        textAlign: "center",
+                      }}
+                    >
+                      {this.state.error.expMonth}
+                    </span>
+                  </div>
+                </div>
+
+                <div className={classes.cardDetailsContainer}>
+                  <div className={classes.inputDiv}>
+                    <span className={classes.sideText}>Expiry Year</span>
+                    <div className={classes.insideInputDiv}>
+                      {" "}
+                      <input
+                        className={classes.input}
+                        type="text"
+                        placeholder="2017"
+                        value={this.state.expYear}
+                        maxLength="4"
+                        name="expYear"
+                        onChange={(e) => this.onChangeHandler(e)}
+                      />
+                      <span
+                        style={{
+                          marginBottom: "5px",
+                          textAlign: "center",
+                          color: "red",
+                        }}
+                      >
+                        {this.state.error.expYear}
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className={classes.inputDiv}>
+                    <span className={classes.sideText}>CVV</span>
+                    <div className={classes.insideInputDiv}>
+                      {" "}
+                      <input
+                        className={classes.input}
+                        type="password"
+                        placeholder="***"
+                        maxLength="3"
+                        name="cvv"
+                        value={this.state.cvv}
+                        onChange={(e) => this.onChangeHandler(e)}
+                      />
+                      <span
+                        style={{
+                          marginBottom: "5px",
+                          textAlign: "center",
+                          color: "red",
+                        }}
+                      >
+                        {this.state.error.cvv}
+                      </span>
+                    </div>
+                  </div>
+
+                  <div></div>
+                </div>
+              </div>
             </div>
-            <div className={classes.inputDiv}>
-              <span className={classes.sideText}>zip code *</span>
-              <input
-                className={classes.input}
-                type="number"
-                name="zipcode"
-                value={this.state.zipcode}
-                onChange={(e) => this.onChangeHandler(e)}
-              />
-            </div>
-            <span style={{ marginBottom: "5px", color: "red" }}>
-              {this.state.error.zipcode}
-            </span>
-            <div className={classes.inputDiv}>
-              <span className={classes.sideText}>Mobile No(with Std) *</span>
-              <input
-                className={classes.input}
-                type="number"
-                name="mobileNo"
-                value={this.state.mobileNo}
-                onChange={(e) => this.onChangeHandler(e)}
-              />
-            </div>
-            <span style={{ marginBottom: "5px", color: "red" }}>
-              {this.state.error.mobileNo}
-            </span>
           </div>
-          <div>
-            <h1>YOUR ORDER</h1>
+
+          <div className={classes.main}>
+            <h1>CART</h1>
             <div className={classes.paymentList}>{List}</div>
             <hr style={{ borderTop: "1px dashed" }} />
             <div className={classes.priceList_items}>
